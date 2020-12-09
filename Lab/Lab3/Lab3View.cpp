d7ec85840df9d6036470ac2cd787c42678071342
Pavle Aleksov <pavle.a@codemancystudio.com>
2020-12-09 21:11:28 +0100

    implemented DrawBottomLeft without grayscale

--- Lab/Lab3/Lab3View.cpp
+++ Lab/Lab3/Lab3View.cpp
@@ -394,6 +394,38 @@ void CLab3View::DrawCenterRight(CDC* pDC)
 
 void CLab3View::DrawBottomLeft(CDC* pDC)
 {
+	DImage dimg;
+	dimg.Load(CString("res/31.dib"));
+	int w = dimg.Width();
+	int h = dimg.Height();
+	CBitmap* bmpImage = dimg.GetCBitmap();
+
+	// transparency
+	CBitmap* mask = MakeImageTransparentAndReturnMask(pDC, bmpImage, w, h);
+
+	XFORM oldTransform;
+	GetWorldTransform(pDC->m_hDC, &oldTransform);
+
+	int prevMode = SetGraphicsMode(pDC->m_hDC, GM_ADVANCED);
+	ModifyWorldTransformTranslate(pDC, -1.45 * gridSquareSize, 10.8 * gridSquareSize, false);
+	ModifyWorldTransformTranslate(pDC, w / 2, h / 2, false);
+	ModifyWorldTransformMirror(pDC, true, true, false);
+	ModifyWorldTransformRotate(pDC, 87, false);
+	ModifyWorldTransformTranslate(pDC, -w / 2, -h / 2, false);
+	CDC* memDC = new CDC();
+	memDC->CreateCompatibleDC(pDC);
+	memDC->SelectObject(mask);
+	pDC->BitBlt(0, 0, w, h, memDC, 0, 0, SRCAND);
+	memDC->SelectObject(bmpImage);
+	pDC->BitBlt(0, 0, w, h, memDC, 0, 0, SRCPAINT);
+
+	memDC->DeleteDC();
+	delete memDC;
+
+	mask->DeleteObject();
+	delete mask;
+	SetWorldTransform(pDC->m_hDC, &oldTransform);
+	SetGraphicsMode(pDC->m_hDC, prevMode);
 }
 
 void CLab3View::DrawBottomMiddle(CDC* pDC)
